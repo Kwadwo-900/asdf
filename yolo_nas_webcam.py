de900dcb34b280003e1cfecc602341884d2cb78a
Kwadwo-900 <sagudze900@gmail.com>
2023-05-09 11:47:58 +0000

    update

--- yolo_nas_webcam.py
+++ yolo_nas_webcam.py
@@ -1,2 +1,10 @@
 import cv2
-from super_gradients.training import Models
+import torch
+from super_gradients.training import models
+from super_gradients.common.objects_names import Models
+
+model = models.get(Models.YOLO_N, pretrained_weights = 'coco')
+
+model = model.to("cuda" if torch.cuda.is_available() else "cpu")
+
+model.predict_webcam()
